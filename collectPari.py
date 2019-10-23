381ac0d7e6a52f98f71891b1c33b30aa4c9b9765
sguney <servetguney@gmail.com>
2019-10-23 16:05:35 +0300

    The 11 Commit

--- collectPari.py
+++ collectPari.py
@@ -20,14 +20,22 @@ def connect_db(client,db,collection):
     except Exception as e:
         print(e)
 
+def make_request(address):
+    try:
+        r = requests.request(method='GET', url=address)
+        array = r.json()
+        print(array)
+    except Exception as e:
+        print(e)
+        time.sleep(5)
+        make_request(address)
+
 if __name__ == '__main__':
     print("Test")
 
     with open('configuration_source.json') as json_file:
         data = json.load(json_file)
         for source in data['DataSources']['Source']:
-            print(source['subentity'])
+            make_request(source['ticker_url'])
     post=connect_db(connect_mongo('127.0.0.1',27017),"pymongo_test","posts")
-    mydict = {"name": "Peter", "address": "Lowstreet 27"}
-    post.insert_one(mydict)
     print(post.find_one())
