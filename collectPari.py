9eb428134caa0d4a471c92988bf9f4c6c8089daa
sguney <servetguney@gmail.com>
2019-10-23 04:38:29 +0300

    İnitial Commit

---
+++ collectPari.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+client = MongoClient('10.8.8.1', 27017)
+db = client.pymongo_test
+posts = db.posts
+
+post_1 = {
+    'title': 'Python and MongoDB',
+    'content': 'PyMongo is fun, you guys',
+    'author': 'Scott'
+}
+post_2 = {
+    'title': 'Virtual Environments',
+    'content': 'Use virtual environments, you guys',
+    'author': 'Scott'
+}
+post_3 = {
+    'title': 'Learning Python',
+    'content': 'Learn Python, it is easy',
+    'author': 'Bill'
+}
+new_result = posts.insert_many([post_1, post_2, post_3])
+print('Multiple posts: {0}'.format(new_result.inserted_ids))
