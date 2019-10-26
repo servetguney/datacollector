8e9ef520e30c89d9a2b0128d327fd29bac66a442
sguney <servetguney@gmail.com>
2019-10-26 11:29:29 +0300

    Commit Ev 18

--- collectPari.py
+++ collectPari.py
@@ -6,19 +6,18 @@ from timeloop import Timeloop
 from datetime import timedelta
 
 
-
-def connect_mongo(ip,port:int):
+def connect_mongo(ip, port:int):
     try:
-        client = MongoClient(ip,port)
+        client = MongoClient(ip, port)
     except Exception as e:
         print(e)
     return client
 
-def connect_db(client,db,collection):
+
+def connect_db(client, db, collection):
     try:
         dbname = client[db]
         coll = dbname[collection]
-        print(coll)
         return coll
     except Exception as e:
         print(e)
@@ -26,58 +25,69 @@ def connect_db(client,db,collection):
 def make_request(address):
     try:
         r = requests.request(method='GET', url=address)
-        print(address)
         array = r.json()
-        print(array)
         return array
     except Exception as e:
         print(e)
         time.sleep(5)
         make_request(address)
 
+
+def add_log(source,log):
+    post = connect_db(connect_mongo('10.8.8.1', 27017), source['database'], 'log')
+    post.insert(log)
+
+
 tl = Timeloop()
+
+
+@tl.job(interval=timedelta(seconds=3650))
+def job_info():
+    with open('configuration_source.json') as json_file:
+        data = json.load(json_file)
+        for source in data['DataSources']['Source']:
+            post = connect_db(connect_mongo('10.8.8.1', 27017), source['database'], 'log')
+            post.find().sort({'$natural':1}).limit(5)
 
 
 @tl.job(interval=timedelta(seconds=86400))
 def job_daily():
     try:
-        print("This is the daily ( 4 Hours ) period")
         with open('configuration_source.json') as json_file:
             data = json.load(json_file)
         for source in data['DataSources']['Source']:
-            print(source)
             if source['type'] == "daily":
                 array = make_request(source['ticker_url'])
                 post = connect_db(connect_mongo('10.8.8.1', 27017), source['database'], source['collection'])
                 mydata = {}
                 mydata['source'] = source['tag']
                 mydata['time'] = time.ctime()
+                mydata['type'] = source['type']
                 if type(array) == list:
                     array = array[0]
                 mydata['data'] = array
-                print(post.insert_one(mydata).inserted_id)
+                post.insert_one(mydata)
     except Exception as e:
         print(e)
 
 
-
 @tl.job(interval=timedelta(seconds=3600))
-def job_daily():
+def job_hourly():
     try:
-        print("This is the 60 sec period")
         with open('configuration_source.json') as json_file:
             data = json.load(json_file)
         for source in data['DataSources']['Source']:
-            if source['type'] == "daily":
+            if source['type'] == "hourly":
                 array = make_request(source['ticker_url'])
                 post = connect_db(connect_mongo('10.8.8.1', 27017), source['database'], source['collection'])
                 mydata = {}
                 mydata['source'] = source['tag']
                 mydata['time'] = time.ctime()
+                mydata['type'] = source['type']
                 if type(array) == list:
                     array = array[0]
                 mydata['data'] = array
-                print(post.insert_one(mydata).inserted_id)
+                post.insert_one(mydata)
     except Exception as e:
         print(e)
 
